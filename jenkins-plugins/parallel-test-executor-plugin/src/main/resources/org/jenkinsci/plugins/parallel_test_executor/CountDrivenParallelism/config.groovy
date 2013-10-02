1ff5e371d876c95941c3e92371add9e091c59dcc
Shay Weiss <shay.weiss@hp.com>
2013-10-02 10:39:20 +0300

    removing duplication of jenkins-plugins (more)

--- jenkins-plugins/parallel-test-executor-plugin/src/main/resources/org/jenkinsci/plugins/parallel_test_executor/CountDrivenParallelism/config.groovy
+++
@@ -1,7 +0,0 @@
-package org.jenkinsci.plugins.parallel_test_executor.CountDrivenParallelism
-
-def f = namespace(lib.FormTagLib)
-
-f.entry(title:"# of parallel tests", field:"size") {
-    f.number()
-}
--- jenkins-plugins/parallel-test-executor-plugin/src/main/resources/org/jenkinsci/plugins/parallel_test_executor/TimeDrivenParallelism/config.groovy
+++
@@ -1,7 +0,0 @@
-package org.jenkinsci.plugins.parallel_test_executor.TimeDrivenParallelism
-
-def f = namespace(lib.FormTagLib)
-
-f.entry(title:"Minutes per execution", field:"mins") {
-    f.number()
-}
